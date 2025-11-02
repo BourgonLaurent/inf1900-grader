e0c2c36e47daa1971a4d69ab6deb743fdb4f782a
Laurent <laurent.bourgon@polymtl.ca>
2025-11-02 15:14:21 -0500

    wip

--- src/models/assemble.py
+++ src/models/assemble.py
@@ -1,3 +1,4 @@
+from enum import Enum, auto
 import re
 from csv import writer
 from datetime import datetime
@@ -9,6 +10,9 @@ from src.models.clone import read_grading_info
 from src.models.grade import generate_grading_file_name, get_teams_list
 from src.models.validate import InvalidInput, ensure_grading_directory_exists, ensure_not_empty, time_format
 
+class AssembleType(Enum):
+    FINAL = auto()
+    PREVIEW = auto()
 
 def parse_grade(number: str) -> Decimal:
     return Decimal(number.strip().replace(',', '.'))
@@ -24,8 +28,8 @@ def sum_partial_grades(team: str, grade_file_path: str) -> Decimal:
     try:
         raw_grades: list[str] = re.findall(PARTIAL_GRADE_REGEX, grading_file_content)
         total_grade: Decimal = sum((parse_grade(grade) for grade in raw_grades), start=Decimal(0))
-    except:
-        raise InvalidInput(f"Missing or invalid partial grade for team {team}.")
+    except Exception as e:
+        raise InvalidInput(f"Missing or invalid partial grade for team {team}.") from e
 
     return total_grade
 
@@ -41,54 +45,105 @@ def write_total_grade(grade_file_path: str, grade: Decimal) -> None:
         f.write(grading_file_content)
 
 
-def extract_total_grade(team: str, grading_directory: str, assignment_sname: str) -> Decimal:
+def extract_total_grade(
+    team: str,
+    grading_directory: str,
+    assignment_sname: str,
+    assemble_type: AssembleType = AssembleType.FINAL,
+) -> Decimal | None:
     repo_path = f"{grading_directory}/{team}"
     grade_file_path = f"{repo_path}/{generate_grading_file_name(assignment_sname)}"
 
-    total_grade = sum_partial_grades(team, grade_file_path)
+    try:
+        total_grade = sum_partial_grades(team, grade_file_path)
+    except InvalidInput:
+        if assemble_type is AssembleType.FINAL:
+            raise
+
+        return None
+
     write_total_grade(grade_file_path, total_grade)
 
     return total_grade
 
 
-def add_grade_to_student_info(student_info: list, grades_map: dict):
-    return {**student_info,
-            'grade': grades_map[student_info["team"]]
-            }
+def add_grade_to_student_info(student_info: dict, grades_map: dict[str, Decimal]) -> dict:
+    return {**student_info, "grade": grades_map[student_info["team"]]}
 
 
-def write_grades_file(grading_directory: str, grades_map: dict[str, Decimal], assignment_sname: str) -> None:
+def write_grades_file(
+    grading_directory: str,
+    grades_map: dict,
+    assignment_sname: str,
+    assemble_type: AssembleType,
+) -> None:
     info = read_grading_info(grading_directory)
     group_number = info["group_number"]
 
     csv_output = StringIO()
-    csv_writer = writer(csv_output, delimiter=';')
-    csv_writer.writerows([
-        ["Cours:", "INF1900"],
-        ["Correcteur:", info["grader_name"]],
-        ["Section:", group_number],
-        ["Date:", datetime.now().strftime(time_format)],
-        ["Travail:", assignment_sname],
-        [],
-        ["Moyenne:", mean(grades_map.values())],
-        ["Écart-type:", stdev(grades_map.values())],
-        [],
-        ["Nom", "Prénom", "Équipe", "Note"],
-        *[list(add_grade_to_student_info(student_info, grades_map).values()) for student_info in info["students"]]
-    ])
+    csv_writer = writer(csv_output, delimiter=";")
+    csv_writer.writerows(
+        [
+            ["Cours:", "INF1900"],
+            ["Correcteur:", info["grader_name"]],
+            ["Section:", group_number],
+            ["Date:", datetime.now().strftime(time_format)],
+            ["Travail:", assignment_sname],
+            [],
+            [
+                "Moyenne:",
+                mean(grades_map.values())
+                if assemble_type is AssembleType.FINAL or len(grades_map) >= 1
+                else "Données insuffisantes",
+            ],
+            [
+                "Écart-type:",
+                stdev(grades_map.values())
+                if assemble_type is AssembleType.FINAL or len(grades_map) >= 2
+                else "Données insuffisantes",
+            ],
+            [],
+            ["Nom", "Prénom", "Équipe", "Note"],
+            *[
+                list(add_grade_to_student_info(student_info, grades_map).values())
+                for student_info in info["students"]
+                if student_info["team"] in grades_map
+            ],
+        ]
+    )
 
     # Hack: replace dot decimals with comma decimals
     csv_output = csv_output.getvalue().replace(".", ",")
 
     grades_path = f"{grading_directory}/notes-inf1900-sect0{group_number}-{assignment_sname}.csv"
+
+    if assemble_type is AssembleType.PREVIEW:
+        grades_path += ".preview"
+
     with open(grades_path, 'w', newline='', encoding="utf-8") as csvfile:
         csvfile.write(csv_output)
 
 
-def assemble(grading_directory: str, assignment_sname: str):
+def assemble(
+    grading_directory: str,
+    assignment_sname: str,
+    assemble_type: AssembleType,
+) -> None:
     ensure_grading_directory_exists(grading_directory)
     ensure_not_empty(assignment_sname, "Assignment short name")
 
     teams = get_teams_list(grading_directory)
-    grades = {team: extract_total_grade(team, grading_directory, assignment_sname) for team in teams}
-    write_grades_file(grading_directory, grades, assignment_sname)
+    grades = {
+        team: grade
+        for team in teams
+        if (
+            grade := extract_total_grade(
+                team,
+                grading_directory,
+                assignment_sname,
+                assemble_type,
+            )
+        )
+        is not None
+    }
+    write_grades_file(grading_directory, grades, assignment_sname, assemble_type)
--- src/views/panels/assemble.py
+++ src/views/panels/assemble.py
@@ -1,18 +1,20 @@
 from urwid import Edit, LineBox
 
-from src.models.assemble import assemble
+from src.models.assemble import AssembleType, assemble
 from src.models.state import state
 from src.views.widgets.form import Form
-
+from src.views.widgets.radio import RadioGroup
 
 class AssemblePanel(Form):
 
     def __init__(self):
         grading_directory = LineBox(Edit(("header", "Grading directory\n\n"), state.grading_directory))
         assignment_sname = LineBox(Edit(("header", "Assignment short name\n\n"), state.assignment_sname))
+        assemble_type = RadioGroup("Assemble type", AssembleType, AssembleType.FINAL)
 
         grid_elements = [
             {"grading_directory": grading_directory, "assignment_sname": assignment_sname},
+            {"assemble_type": assemble_type},
         ]
 
         super().__init__("Assemble", grid_elements, assemble)
